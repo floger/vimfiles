082c91e256f9dbf94bde35bcb6caf0aff6f6b81b
Deshi Xiao <xiaods@gmail.com>
2011-12-10 13:54:44 +0800

    add vim indent plugin

--- install.sh
+++ install.sh
@@ -7,5 +7,8 @@ echo "Installing vimfiles..."
 
 cp ~/.vimrc ~/.vimrc.old > /dev/null
 rm ~/.vimrc > /dev/null
+cp ~/.vimrc.local ~/.vimrc.local.old > /dev/null
+rm ~/.vimrc.local
 
 ln -s ~/.vim/vimrc ~/.vimrc
+ln -s ~/.vim/vimrc.local ~/.vimrc.local
